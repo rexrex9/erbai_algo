d9c1b1e5dc36b7792056c237a8070d8d97ac6182
rexrex9 <547102444@qq.com>
2023-07-27 22:49:53 +0800

    init

---
+++ trys/try_index.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import os,sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+face_path = 'algo_medias/test_files/face1.jpg'
+
+def try_index():
+
+    print("try_index")
+    pass
