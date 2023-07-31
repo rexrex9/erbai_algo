69cd45083cdc56a7701045178e079eb10ea1fd03
rexrex9 <547102444@qq.com>
2023-07-31 22:51:45 +0800

    init

--- trys/try_wav2lip.py
+++ trys/try_wav2lip.py
@@ -7,7 +7,7 @@ face_path = 'algo_medias/test_files/face1.jpg'
 video_path = 'algo_medias/test_files/test_video_13s.mp4'
 audio_path = 'algo_medias/test_files/wav2lip_audio.WAV'
 host = 'http://172.17.0.2'
-host = 'http://116.62.176.131'
+#host = 'http://116.62.176.131'
 def try_image():
     url = host +':8999/do'
     d = {'oss_media_path':face_path,audio_path:audio_path,'out_format':'png','ifoss':True}
